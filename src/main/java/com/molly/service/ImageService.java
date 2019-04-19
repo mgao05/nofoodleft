36578d4bbe30d2a80f55ebed9ff620ee65a11e65
Mengran <mengrangao@gwu.edu>
2019-04-18 23:46:33 -0400

    add to ImageService

--- src/main/java/com/molly/service/ImageService.java
+++ src/main/java/com/molly/service/ImageService.java
@@ -1,5 +1,6 @@
 package com.molly.service;
 
+import com.amazonaws.services.s3.model.S3Object;
 import com.molly.domain.FilenameUtils;
 import com.molly.domain.Image;
 import com.molly.repository.ImageRepository;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.Object.*;
 
 @Service
@@ -21,16 +24,31 @@ public class ImageService {
     private ImageRepository imageRepository;
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public static String getExtension(String filename){
-        
-    }
+
     @Transactional
-    public Image saveFakeImage(MultipartFile multipartFile, boolean isPublic)throws ServiceException {
+
+    public Image saveFakeImage(MultipartFile multipartFile, boolean isPublic)throws ServiceException{
         if(multipartFile == null || multipartFile.isEmpty())throw new ServiceException("File must be valid");
 
-
-
         String extension = FilenameUtils.getExtension(multipartFile.getOriginalFilename());
+
+        // String homeDir = System.getProperty("cataline.base") !=null ? System.getProperty("cataline.base");
+        Image image = new Image();
+        String s3Key = FilenameUtils.getBaseName(multipartFile.getOriginalFilename())+ "_"+image.getUuid()+image.getExtension();
+        File localFile = new File(homeDir + s3Key);
+
+        try{
+            multipartFile.transferTo(localFile);
+            storageService.putObject(s3Key,localFile);
+            S3Object s3Object = storageService.getObject(s3Key);
+            image.setUrl(storageService.getObjectUrl(s3Object.getKey()));
+            image.setExtension(extension);
+            return image;
+        }catch (IOException e){
+            logger.warn("can't find image file");
+        }
+        return null;
+
 
     }
 
